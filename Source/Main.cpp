1edf0c6b304032ae73bf55cfea30d7f7d06823db
Piotr Doan <doanpiotr@gmail.com>
2015-07-22 03:23:51 +0200

    Create an application window.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -30,5 +30,49 @@ int main(int argc, char* argv[])
         glfwTerminate();
     };
 
+    // Create a window.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1024, 576, "Game", nullptr, nullptr);
+
+    if(window == nullptr)
+    {
+        Log() << "Failed to create a window!";
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    BOOST_SCOPE_EXIT(&)
+    {
+        glfwDestroyWindow(window);
+    };
+
+    // Check created OpenGL context.
+    int glMajor = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MAJOR);
+    int glMinor = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MINOR);
+
+    Log() << "Created OpenGL " << glMajor << "." << glMinor << " context.";
+
+    // Main loop.
+    while(!glfwWindowShouldClose(window))
+    {
+        // Clear the back buffer.
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearDepth(1.0f);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Swap the back buffer.
+        glfwSwapInterval(1);
+        glfwSwapBuffers(window);
+
+        // Process window events.
+        glfwPollEvents();
+    }
+
     return 0;
 }
