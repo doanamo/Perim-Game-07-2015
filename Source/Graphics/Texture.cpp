38c88793ec3e239b0053b56aea1c8f6f8e465ed9
Piotr Doan <doanpiotr@gmail.com>
2015-08-20 23:41:13 +0200

    Import texture class.

---
+++ Source/Graphics/Texture.cpp
@@ -0,0 +1,103 @@
+#include "Precompiled.hpp"
+#include "Texture.hpp"
+using namespace Graphics;
+
+namespace
+{
+    // Log error messages.
+    #define LogInitializeError() "Failed to initialize a texture! "
+    
+    // Invalid types.
+    const GLuint InvalidHandle = 0;
+}
+
+Texture::Texture() :
+    m_handle(InvalidHandle),
+    m_format(GL_INVALID_ENUM),
+    m_width(0),
+    m_height(0),
+    m_initialized(false)
+{
+}
+
+Texture::~Texture()
+{
+    // Free the texture handle.
+    if(m_handle != InvalidHandle)
+    {
+        glDeleteTextures(1, &m_handle);
+    }
+}
+
+bool Texture::Initialize(int width, int height, GLenum format, const void* data)
+{
+    BOOST_ASSERT(!m_initialized);
+
+    // Validate arguments.
+    if(width <= 0)
+    {
+        Log() << LogInitializeError() << "Invalid argument - \"width\" is invalid.";
+        return false;
+    }
+
+    if(height <= 0)
+    {
+        Log() << LogInitializeError() << "Invalid argument - \"height\" is invalid.";
+        return false;
+    }
+
+    m_width = width;
+    m_height = height;
+    m_format = format;
+
+    // Create a texture handle.
+    glGenTextures(1, &m_handle);
+
+    if(m_handle == InvalidHandle)
+    {
+        Log() << LogInitializeError() << "Couldn't create a texture.";
+        return false;
+    }
+
+    // Bind the texture.
+    glBindTexture(GL_TEXTURE_2D, m_handle);
+
+    // Set default sampling parameters.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+ 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Set packing aligment for provided data.
+    /*
+    if(format == GL_RED)
+    {
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    }
+    */
+
+    // Allocated a texture surface on the hardware.
+    glTexImage2D(GL_TEXTURE_2D, 0, format, m_width, m_height, 0, format, GL_UNSIGNED_BYTE, data);
+
+    // Unbind the texture.
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return true;
+}
+
+void Texture::Update(const void* data)
+{
+    BOOST_ASSERT(m_initialized);
+    BOOST_ASSERT(data != nullptr);
+
+    // Upload new texture data.
+    glBindTexture(GL_TEXTURE_2D, m_handle);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_width, m_height, m_format, GL_UNSIGNED_BYTE, data);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+bool Texture::IsValid() const
+{
+    return m_initialized;
+}
---
+++ Source/Graphics/Texture.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "Precompiled.hpp"
+
+//
+// Texture
+//
+
+namespace Graphics
+{
+    // Texture class.
+    class Texture
+    {
+    public:
+        Texture();
+        ~Texture();
+
+        // Initializes the texture.
+        bool Initialize(int width, int height, GLenum format, const void* data);
+
+        // Updates the texture data.
+        void Update(const void* data);
+
+        // Gets the texture handle.
+        GLuint GetHandle() const
+        {
+            return m_handle;
+        }
+
+        // Gets the texture width.
+        int GetWidth() const
+        {
+            return m_width;
+        }
+
+        // Gets the texture height.
+        int GetHeight() const
+        {
+            return m_height;
+        }
+
+        // Checks if instance is valid.
+        bool IsValid() const;
+
+    private:
+        // Texture handle.
+        GLuint m_handle;
+        GLenum m_format;
+
+        // Texture parameters.
+        int m_width;
+        int m_height;
+
+        // Initialization state.
+        bool m_initialized;
+    };
+}
