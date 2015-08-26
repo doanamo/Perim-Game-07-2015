8efe1d2e2190753cdebc190a47a7c3c5bc14f08b
Piotr Doan <doanpiotr@gmail.com>
2015-08-26 14:30:36 +0200

    Extend render component to hold a texture resource.

--- Source/Game/Components/Render.cpp
+++ Source/Game/Components/Render.cpp
@@ -2,13 +2,16 @@
 #include "Render.hpp"
 #include "Transform.hpp"
 #include "Game/ComponentSystem.hpp"
+#include "Graphics/Texture.hpp"
 using namespace Game;
 using namespace Components;
 
 Render::Render() :
+    m_rectangle(0.0f, 0.0f, 1.0f, 1.0f),
     m_diffuseColor(1.0f, 1.0f, 1.0f, 1.0f),
     m_emissiveColor(1.0f, 1.0f, 1.0f, 1.0f),
     m_emissivePower(0.0f),
+    m_transparent(true),
     m_transform(nullptr)
 {
 }
@@ -30,6 +33,21 @@ bool Render::Finalize(EntityHandle self, const Context& context)
     return true;
 }
 
+glm::vec4 Render::CalculateColor() const
+{
+    return glm::mix(m_diffuseColor, m_emissiveColor, m_emissivePower);
+}
+
+void Render::SetTexture(TexturePtr& texture)
+{
+    m_texture = texture;
+}
+
+void Render::SetRectangle(const glm::vec4& rectangle)
+{
+    m_rectangle = rectangle;
+}
+
 void Render::SetDiffuseColor(const glm::vec4& color)
 {
     m_diffuseColor = color;
@@ -45,9 +63,24 @@ void Render::SetEmissivePower(float power)
     m_emissivePower = power;
 }
 
+void Render::SetTransparent(bool transparent)
+{
+    m_transparent = transparent;
+}
+
 Transform* Render::GetTransform()
 {
     return m_transform;
+}
+
+const Render::ConstTexturePtr& Render::GetTexture() const
+{
+    return m_texture;
+}
+
+const glm::vec4& Render::GetRectangle() const
+{
+    return m_rectangle;
 }
 
 const glm::vec4& Render::GetDiffuseColor() const
@@ -63,4 +96,9 @@ const glm::vec4& Render::GetEmissiveColor() const
 float Render::GetEmissivePower() const
 {
     return m_emissivePower;
+}
+
+bool Render::IsTransparent() const
+{
+    return m_transparent;
 }
--- Source/Game/Components/Render.hpp
+++ Source/Game/Components/Render.hpp
@@ -4,6 +4,12 @@
 #include "Game/Component.hpp"
 #include "Game/EntityHandle.hpp"
 
+// Forward declarations.
+namespace Graphics
+{
+    class Texture;
+}
+
 //
 // Render Component
 //
@@ -19,11 +25,25 @@ namespace Game
         class Render : public Component
         {
         public:
+            // Type declarations.
+            typedef std::shared_ptr<Graphics::Texture> TexturePtr;
+            typedef std::shared_ptr<const Graphics::Texture> ConstTexturePtr;
+
+        public:
             Render();
             ~Render();
 
-            // Finalizes render component.
+            // Finalizes the render component.
             bool Finalize(EntityHandle self, const Context& context) override;
+
+            // Calculates the final color.
+            glm::vec4 CalculateColor() const;
+
+            // Sets the texture.
+            void SetTexture(TexturePtr& texture);
+
+            // Sets the rectangle.
+            void SetRectangle(const glm::vec4& rectangle);
 
             // Set the diffuse color.
             void SetDiffuseColor(const glm::vec4& color);
@@ -34,8 +54,17 @@ namespace Game
             // Set the emissive power.
             void SetEmissivePower(float power);
 
+            // Sets transparency state.
+            void SetTransparent(bool transparent);
+
             // Gets the transform component.
             Transform* GetTransform();
+
+            // Gets the texture.
+            const ConstTexturePtr& GetTexture() const;
+
+            // Gets the rectangle.
+            const glm::vec4& GetRectangle() const;
 
             // Gets the diffuse color.
             const glm::vec4& GetDiffuseColor() const;
@@ -46,11 +75,19 @@ namespace Game
             // Gets the emissive power.
             float GetEmissivePower() const;
 
+            // Checks if is transparent.
+            bool IsTransparent() const;
+
         private:
+            // Texture resource.
+            ConstTexturePtr m_texture;
+            glm::vec4 m_rectangle;
+
             // Render parameters.
             glm::vec4 m_diffuseColor;
             glm::vec4 m_emissiveColor;
             float m_emissivePower;
+            bool m_transparent;
 
             // Entity components.
             Transform* m_transform;
