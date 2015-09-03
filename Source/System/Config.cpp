5944f7d6e777e94c3c32e4996440bc8b497ec508
Piotr Doan <doanpiotr@gmail.com>
2015-09-03 19:04:06 +0200

    Implement Config class using Lua C API.

--- Source/System/Config.cpp
+++ Source/System/Config.cpp
@@ -10,6 +10,7 @@ namespace
 }
 
 Config::Config() :
+    m_lua(nullptr),
     m_initialized(false)
 {
 }
@@ -22,6 +23,13 @@ Config::~Config()
 
 void Config::Cleanup()
 {
+    // Cleanup Lua state.
+    if(m_lua != nullptr)
+    {
+        lua_close(m_lua);
+        m_lua = nullptr;
+    }
+
     // Reset initialization state.
     m_initialized = false;
 }
@@ -38,19 +46,8 @@ bool Config::Initialize()
             this->Cleanup();
     };
 
-    /*
     // Create Lua state.
-    try
-    {
-        m_context = LuaContext(false);
-    }
-    catch(const LuaException& exception)
-    {
-        Log() << LogInitializeError() << "An exception occured.";
-        Log() << "Lua Error: " << exception.what();
-        return false;
-    }
-    */
+    m_lua = luaL_newstate();
 
     // Success!
     return m_initialized = true;
@@ -74,29 +71,24 @@ bool Config::Load(std::string filename)
             this->Cleanup();
     };
 
-    /*
-    // Load the config file.
-    try
+    // Load the config file
+    if(luaL_dofile(m_lua, (Build::GetWorkingDir() + filename).c_str()) != 0)
     {
-        // Parse the config file.
-        m_context.doFile(Build::GetWorkingDir() + filename);
-
-        // Make sure config table is present.
-        LuaRef reference = m_context.getGlobal("Config");
-
-        if(reference == nullptr)
-        {
-            Log() << LogLoadError(filename) << "Missing \"Config\" table.";
-            return false;
-        }
-    }
-    catch(const LuaException& exception)
-    {
-        Log() << LogLoadError(filename) << "An exception occured.";
-        Log() << "Lua Error: " << exception.what();
+        Log() << LogLoadError(filename) << "Couldn't parse the file.";
+        Log() << "Lua Error: " << lua_tostring(m_lua, -1);
         return false;
     }
-    */
+
+    // Validate state.
+    lua_getglobal(m_lua, "Config");
+
+    if(!lua_istable(m_lua, -1))
+    {
+        Log() << LogLoadError(filename) << "Missing \"Config\" table.";
+        return false;
+    }
+
+    lua_pop(m_lua, 1);
     
     // Success!
     Log() << "Loaded a config from \"" << filename << "\" file.";
@@ -104,43 +96,55 @@ bool Config::Load(std::string filename)
     return success = true;
 }
 
-/*
-LuaRef Config::Resolve(std::string name)
+void Config::PushReference(std::string name)
 {
-    if(!m_initialized)
-        return LuaRef();
+    assert(m_initialized);
 
-    if(name.empty())
-        return LuaRef();
+    // Parse name tokens.
+    auto tokens = Utility::Split(name, '.');
 
-    // Get the token that we will start from.
-    LuaRef token = m_context.globals().get("Config");
-
-    // Resolve reference.
-    auto tokenBegin = name.begin();
-
-    while(true)
+    if(tokens.empty())
     {
-        // Get the token ending iterator.
-        auto tokenEnd = std::find(tokenBegin, name.end(), '.');
-
-        if(tokenBegin == tokenEnd)
-            return LuaRef();
-
-        // Advance token reference deeper into the table.
-        token = token[std::string(tokenBegin, tokenEnd)];
-
-        if(token == nullptr)
-            return LuaRef();
-
-        // Advance the token beginning iterator.
-        if(tokenEnd == name.end())
-            break;
-
-        tokenBegin = tokenEnd + 1;
+        // Push a nil pair.
+        lua_pushnil(m_lua);
+        lua_pushnil(m_lua);
+        return;
     }
 
-    // Return resolved reference.
-    return token;
+    // Push config table pair.
+    lua_pushvalue(m_lua, LUA_GLOBALSINDEX);
+    lua_pushstring(m_lua, "Config");
+
+    // Traverse the table chain.
+    for(const std::string& token : tokens)
+    {
+        // Get table element.
+        lua_gettable(m_lua, -2);
+
+        // Remove previous table.
+        lua_remove(m_lua, -2);
+
+        // Check if we got a table.
+        if(!lua_istable(m_lua, -1))
+        {
+            lua_pop(m_lua, 1);
+
+            // Push a nil pair.
+            lua_pushnil(m_lua);
+            lua_pushnil(m_lua);
+            return;
+        }
+
+        // Push token key.
+        lua_pushstring(m_lua, token.c_str());
+    }
 }
-*/
+
+void Config::PushValue()
+{
+    assert(m_initialized);
+
+    // Push value and remove table.
+    lua_gettable(m_lua, -2);
+    lua_remove(m_lua, -2);
+}
--- Source/System/Config.hpp
+++ Source/System/Config.hpp
@@ -33,15 +33,59 @@ namespace System
         Type Get(std::string name, const Type& default);
 
     private:
-        /*
-        // Resolves a reference name.
-        LuaRef Resolve(std::string name);
-        */
+        // Resolves variable name and pushes a table/key pair that referes to it on the stack.
+        void PushReference(std::string name);
+
+        // Pushes a value and consumes the table/key pair in the process.
+        void PushValue();
+
+        // Casts a value and consumes it in the process.
+        template<typename Type>
+        Type CastValue(const Type& default);
 
     private:
+        // Lua state.
+        lua_State* m_lua;
+        
         // Initialization state.
         bool m_initialized;
     };
+
+    template<>
+    inline bool Config::CastValue<bool>(const bool& default)
+    {
+        bool value = default;
+
+        // Cast the value.
+        if(lua_isboolean(m_lua, -1))
+        {
+            value = lua_toboolean(m_lua, -1) != 0;
+        }
+
+        // Remove from the stack.
+        lua_pop(m_lua, 1);
+
+        // Return the value.
+        return value;
+    }
+
+    template<>
+    inline int Config::CastValue<int>(const int& default)
+    {
+        int value = default;
+
+        // Cast the value.
+        if(lua_isnumber(m_lua, -1))
+        {
+            value = (int)std::round(lua_tonumber(m_lua, -1));
+        }
+
+        // Remove from the stack.
+        lua_pop(m_lua, 1);
+
+        // Return the value.
+        return value;
+    }
 
     template<typename Type>
     void Config::Set(std::string name, const Type& value)
@@ -64,21 +108,13 @@ namespace System
         if(!m_initialized)
             return default;
 
-        /*
-        // Resolve reference.
-        LuaRef reference = this->Resolve(name);
+        // Push value reference.
+        this->PushReference(name);
 
-        // Get the value.
-        try
-        {
-            return reference.toValue<Type>();
-        }
-        catch(...)
-        {
-            return default;
-        }
-        */
+        // Push value itself.
+        this->PushValue();
 
-        return default;
+        // Cast and return the value.
+        return this->CastValue(default);
     }
 };
